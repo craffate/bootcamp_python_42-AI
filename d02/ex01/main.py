ede264cbc91524284335bb3f50be943565fca530
Cyril Raffatelli <craffate@student.42.fr>
2019-11-06 13:37:09 +0100

    Add d02/ex01

---
+++ d02/ex01/main.py
@@ -0,0 +1,36 @@
+def what_are_the_vars(*args, **kwargs):
+    ret = ObjectC()
+    idx = 0
+    for k, v in kwargs.items():
+        setattr(ret, k, v)
+    for i in enumerate(args):
+        setattr(ret, f"var_{idx}", i)
+        idx = idx + 1
+    return ret
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
